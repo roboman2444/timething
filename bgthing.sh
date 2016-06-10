c71d3db6251b74588849dc97e1ce5d6830f5c90f
roboman2444 <roboman2444@gmail.com>
2016-06-10 13:04:24 -0400

    added thing for on the hour

---
+++ bgthing.sh
@@ -0,0 +1,17 @@
+#/bin/sh
+
+while : ;
+do
+	DOUT=$(date +%M.%S)
+	MIN=$(echo $DOUT | cut -d '.' -f1)
+	SEC=$(echo $DOUT | cut -d '.' -f2)
+	TOSEC=$((3600-(SEC + (MIN * 60))))
+## put stuff you want to run close to every hour on the hour below here
+	FILE=$(find /home/roboman2444/wallpapers/ /mnt/thing1/home/roboman2444/wallpapers/ -type f | shuf -n1)
+	feh --bg-fill "$FILE"
+#	echo $FILE $MIN $SEC $TOSEC
+
+#ABOVE HERE
+	sleep $TOSEC
+done
+
