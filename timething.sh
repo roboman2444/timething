79d2226375b555f5a9e9ebab83b5f04450f8c30e
roboman2444 <roboman2444@gmail.com>
2016-06-15 13:59:26 -0400

    added power

--- timething.sh
+++ timething.sh
@@ -8,7 +8,8 @@ do
 	TOSEC=$((60-SEC))
 ## put stuff you want to run close to every minute on the minute below here
 	LOAD=$(cat /proc/loadavg)
-	xsetroot -name "`echo $LOAD | cut -d ' ' -f1` `echo $LOAD | cut -d ' ' -f4` $HM"
+	POWER=$(acpi)
+	xsetroot -name "`echo $POWER | cut -d ' ' -f4 | cut -d ',' -f1` `echo $LOAD | cut -d ' ' -f1` `echo $LOAD | cut -d ' ' -f4` $HM"
 ##	echo $HM $SEC $TOSEC
 
 #ABOVE HERE
