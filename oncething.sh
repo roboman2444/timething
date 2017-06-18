451d007823881f199a54f6bb049767dfcfc2ec56
roboman2444 <roboman2444@gmail.com>
2017-06-18 00:58:06 -0400

    fixed the oncething issue

--- oncething.sh
+++ oncething.sh
@@ -4,7 +4,7 @@
 	SHUFSTRING=$(find /home/roboman2444/wallpapers/ /mnt/thing1/home/roboman2444/wallpapers/ -type f | shuf -n $NUMMONITORS)
 	FEHSTRING=
 ##	for i in $(seq 1 $NUMMONITORS);
-	for FILE in "${SHUFSTRING[@]"
+	for FILE in "${SHUFSTRING[@]}";
 	do
 ##		FILE=$(find /home/roboman2444/wallpapers/ /mnt/thing1/home/roboman2444/wallpapers/ -type f | shuf -n1)
 		FEHSTRING=$(printf "%s --bg-fill %s" "$FEHSTRING" "$FILE")
